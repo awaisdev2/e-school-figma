ae3987d863c819489ad11d449078d4fd6047499d
Awais <awaisdev2@gmail.com>
2024-02-08 23:35:17 +0500

    add 3d hover animation cards

--- src/components/AboutCards.jsx
+++ src/components/AboutCards.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
+import React from "react";
+import Atropos from "atropos/react";
 
-import image1 from './media/image1.png'
-import image2 from './media/image2.png'
-import image3 from './media/image3.png'
-import image4 from './media/image4.png'
+import image1 from "./media/image1.png";
+import image2 from "./media/image2.png";
+import image3 from "./media/image3.png";
+import image4 from "./media/image4.png";
 
 const AboutCards = (props) => {
-    return (
-        <div className={`${props.bg} rounded-md flex flex-col items-center justify-center sm:w-[270px] h-[270px] m-5`}>
-            <img className='h-20' src={props.img} alt="img" />
-            <p className='text-[#413960] text-xl font-medium mt-3'>{props.title}</p>
-            <p className='text-[#585859] text-base'>Learn Anythings</p>
-        </div>
-      )
-}
+  return (
+    <Atropos rotateX={45} rotateY={45}>
+      <div
+        className={`${props.bg} rounded-md flex flex-col items-center justify-center sm:w-[270px] h-[270px] m-5`}
+      >
+        <img className="h-20" src={props.img} alt="img" />
+        <p className="text-[#413960] text-xl font-medium mt-3">{props.title}</p>
+        <p className="text-[#585859] text-base">Learn Anythings</p>
+      </div>
+    </Atropos>
+  );
+};
 
 const AboutCardsIndex = () => {
   return (
     <div className='block sm:flex sm:justify-center'>
       <AboutCards bg='bg-[#F8FFFB]' img={image1} title='1500+ Topic' />
-      <AboutCards bg='bg-[white] shadow-xl' img={image2} title='1800+ Students' />
+      <AboutCards bg='bg-[white]' img={image2} title='1800+ Students' />
       <AboutCards bg='bg-[#F8FFFB]' img={image3} title='9K+ Test Token' />
       <AboutCards bg='bg-[#F8FFFB]' img={image4} title='2000+ Student' />
     </div>
